c4fcae34a310d6f92596c249260e7456b64eac19
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-15 13:54:41 -0500

    Completed user can delete a task action#destroy modified view of index

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
   # update a task
   get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch '/tasks/:id', to: 'tasks#update'
+
+  # delete a task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
